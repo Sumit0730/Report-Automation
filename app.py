4f0e08e647955fbcb32b21a97ba4bc35a115643f
Krongreap <sm924533@gmail.com>
2025-02-21 23:30:50 +0530

    charts data modification

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ date_to=d[1]
 week = d[2]
 month=d[3]
 
-
 df = {
     'Total Users': [6767676],
     'Mobile Users': [44423],
@@ -29,29 +28,29 @@ colleges_data=pd.DataFrame(df)
 # level=level_count()
 
 
-data = {'Totalusers':colleges_data['Total Users'].values[0],
-        'MobileUsers':colleges_data['Mobile Users'].values[0],
-        'DesktopUsers':colleges_data['Desktop Users'].values[0],
+data = {'Totalusers':int(np.int64(colleges_data['Total Users'].values[0])),
+        'MobileUsers':int(np.int64(colleges_data['Mobile Users'].values[0])),
+        'DesktopUsers':int(np.int64(colleges_data['Desktop Users'].values[0])),
         'MobileVerifyUsers':int(np.int64(colleges_data['Mobile Verify Users'].values[0])),
-        'Desktopverifyusers':colleges_data['Desktop verify users'].values[0],
-        'ugcount':colleges_data['UG Count'].values[0],
-        'pgcount':colleges_data['PG Count'].values[0],
+        'Desktopverifyusers':int(np.int64(colleges_data['Mobile Users'].values[0])),
+        'ugcount':int(np.int64(colleges_data['UG Count'].values[0])),
+        'pgcount':int(np.int64(colleges_data['PG Count'].values[0])),
         'month':month,
         'week_name':week,
         'date_from': date_from,
         'date_to':date_to}
 
-with open("C:\\Users\\sm924\Desktop\\Registration report\\templates\\template2.html", "r", encoding="utf-8") as file:
+with open("C:\\Users\\sm924\\Desktop\\Final_Automation\\Report-Automation\\templates\\index.html", "r", encoding="utf-8") as file:
     html_template = file.read()
 
 template = Template(html_template)
 html_content = template.render(data)
 
 # Save to an HTML file
-with open("C:\\Users\\sm924\Desktop\\Registration report\\templates\\output.html", "w") as file:
+with open("C:\\Users\\sm924\\Desktop\\Final_Automation\\Report-Automation\\templates\\final.html", "w") as file:
     file.write(html_content)
 print('HTML File Updated........')
 
-report_path = "C:\\Users\\sm924\Desktop\\Registration report\\templates\\output.html"
+report_path = "C:\\Users\\sm924\\Desktop\\Final_Automation\\Report-Automation\\templates\\final.html"
 send_email(report_path)
 print("Report generated and sent successfully!")
